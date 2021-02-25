560d147467213bb658f6dc6f99770fa8fff8f6ba
jingyueshi2009 <jingyueshi2009@sina.com>
2021-02-25 12:17:28 +0800

    update project

---
+++ mztespro/guest/test_case/models/driver.py
@@ -0,0 +1,18 @@
+# -*- coding utf-8 -*-
+# @author:"zhangJingHua"
+
+from selenium.webdriver import Remote
+from selenium import webdriver
+
+#启动浏览器
+def browser():
+    #driver=webdriver.Chrome()
+    host='127.0.0.1:8001'  #运行主机：端口号（本机默认：127.0.0.1:4444）
+    dc={'browserName':'chrome'} #指定浏览器（'chrome','firefox'）
+    driver=Remote(command_executor='http://'+host+'/wd/hub',desired_capabilities=dc)
+    return driver
+
+if __name__=='__main__':
+    dr=browser()
+    dr.get("http://www.baidu.com")
+    dr.quit()
